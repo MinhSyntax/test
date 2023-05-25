459299277a9802c677b2c6172216f9245955bfdd
Minh Vuong <minhtv@web.de>
2023-05-25 20:33:07 +0200

    Minh ist der coolste

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, Minh ist der coolste")
+            Text("Minh ist der coolste")
         }
         .padding()
     }
