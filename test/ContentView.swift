dff05bc84532ef016ca04e10397df9e7dfc1e132
Minh Vuong <minhtv@web.de>
2023-05-25 20:27:07 +0200

    neue Änderung

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, Minh ist der coolste")
         }
         .padding()
     }
