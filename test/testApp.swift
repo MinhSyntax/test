3713caf37799f794dabe09d5f3c4051440733ead
Minh Vuong <minhtv@web.de>
2023-05-25 20:10:45 +0200

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Man Minni on 25.05.23.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
